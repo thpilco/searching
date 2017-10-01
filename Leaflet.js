75314c89a3da514b36bb63737408a07988673ebf
Elizabeth Pilco <elizabethpilco@hotmail.com>
2017-10-01 21:33:30 +0200

    Hoja de estilo

---
+++ Leaflet.js
@@ -0,0 +1,24 @@
+var mymap = L.map('map').setView([37.185669, -3.610582], 17);
+
+	L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+		maxZoom: 20,
+		attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+			'<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+			'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+		id: 'mapbox.streets'
+	}).addTo(mymap);
+
+	L.marker([37.185669, -3.610582]).addTo(mymap)
+		.bindPopup("<b>Punto geolocalizado</b><br />37.185669 -3.610582").openPopup();
+
+
+	var popup = L.popup();
+
+	function onMapClick(e) {
+		popup
+			.setLatLng(e.latlng)
+			.setContent("You clicked the map at " + e.latlng.toString())
+			.openOn(mymap);
+	}
+
+	mymap.on('click', onMapClick);

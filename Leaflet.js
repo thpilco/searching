28b997e0a5b097e1b15ac6a91c88d6c9be55a925
Antonio Ángel Morales Parejo <anzony_mp@hotmail.es>
2017-10-02 15:33:58 +0200

    Otras capas

--- Leaflet.js
+++ Leaflet.js
@@ -63,8 +63,15 @@ var ggl = new L.Google('ROADMAP');
 
 mymap.addLayer(ggl);*/
 
+var mymap = L.map('map', {
+	center: [37.1572788, -3.6020432,15],
+        minZoom: 15,
+        maxZoom: 18,
+	zoom: 15,
+        dragging: "false"
+});
+/*
 var Bar = L.layerGroup();
-
 
 var mbAttr = 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
 		'<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
@@ -73,27 +80,58 @@ var mbAttr = 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</
 
 var grayscale   = L.tileLayer(mbUrl, {id: 'mapbox.light', attribution: mbAttr}),
     streets  = L.tileLayer(mbUrl, {id: 'mapbox.streets',   attribution: mbAttr});
-
-var mymap = L.map('map', {
-	center: [37.1572788, -3.6020432,15],
-        minZoom: 15,
-        maxZoom: 18,
-	zoom: 15,
-	layers: [grayscale, Bar]
+    */
+// Crea capa openstreetmap
+var osm = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '&copy <a href="http://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a> contributors'
 });
 
-var baseLayers = {
-	"Escala de grises": grayscale,
-	"Calles": streets
+// Crea capa stamen lite
+var lite = L.tileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}.png', {
+  attribution: 'Map tiles by <a href="http://stamen.com" target="_blank">Stamen Design</a>, ' +
+    '<a href="http://creativecommons.org/licenses/by/3.0" target="_blank">CC BY 3.0</a> &mdash; ' +
+    'Map data &copy <a href="http://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a> contributors'
+});
+
+// Crea capa imagen de esri
+var esri_img = L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+  attribution: 'Tiles &copy; &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+});
+
+// Crea capa OpenWeatherMap nubes
+var owm_clouds = L.tileLayer('http://{s}.tile.openweathermap.org/map/clouds/{z}/{x}/{y}.png', {
+  opacity: 0.75, 
+  attribution: 'Map data &copy; <a href="http://openweathermap.org">OpenWeatherMap</a>'
+});
+
+// Crea capa OpenWeatherMap presión
+var owm_pressure_cntr = L.tileLayer('http://{s}.tile.openweathermap.org/map/pressure_cntr/{z}/{x}/{y}.png', {
+  attribution: 'Map data &copy; <a href="http://openweathermap.org">OpenWeatherMap</a>'
+});
+
+/*var baseLayers = {
+    "Escala de grises": grayscale,
+    "Calles": streets,
+    "OpenStreetMap": osm,
+    "Stamen toner-lite": lite,
+    "Satélite": esri_img
 };
 
 var overlays = {
 	"Bar": Bar
 };
+*/
+//mymap.dragging.disable(baseLayers);
 
-mymap.dragging.disable(baseLayers);
+//L.control.layers(baseLayers, overlays).addTo(mymap);
 
-L.control.layers(baseLayers, overlays).addTo(mymap);
+L.control.layers({ // Capas base
+  "OpenStreetMap": osm,
+  "Stamen toner-lite": lite,
+  "Satélite": esri_img
+}, { // Capas de superposición
+    "OpenWeatherMap nubes": owm_clouds,
+  "OpenWeatherMap presión": owm_pressure_cntr
+}).addTo(mymap);
 
 var popup = L.popup();
-
